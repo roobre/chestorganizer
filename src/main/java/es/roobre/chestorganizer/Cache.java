e6ba38f9203b33a4de1b4af65b9ff1babf2988fa
Roberto Santalla <roobre@roobre.es>
2019-07-07 13:11:17 +0200

    Make cache expire after certain time (30 min)

--- src/main/java/es/roobre/chestorganizer/Cache.java
+++ src/main/java/es/roobre/chestorganizer/Cache.java
@@ -10,31 +10,49 @@ import java.util.Map;
  * Caches the best receivers for a given organizer and material
  * TODO: This cache is only invalidated if a returned location is no longer valid, not if it is no longer the best. This is not optimal.
  */
-public class Cache {
-    private Map<Location, Map<Material, Location>> holder;
+class Cache {
+    private static long MAX_AGE = 30 * 60 * 1000; // 30 minutes
+    private Map<Location, Map<Material, CacheItem<Location>>> holder;
 
-    public Cache() {
+    Cache() {
         this.holder = new HashMap<>();
     }
 
-    public synchronized Location get(Location organizerLocation, Material material) {
-        Map<Material, Location> materialMap = this.holder.get(organizerLocation);
+    synchronized Location get(Location organizerLocation, Material material) {
+        Map<Material, CacheItem<Location>> materialMap = this.holder.get(organizerLocation);
         if (materialMap != null) {
-            return materialMap.get(material);
+            CacheItem<Location> item = materialMap.get(material);
+            if (item.time + MAX_AGE > System.currentTimeMillis()) {
+                // Still valid
+                return item.item;
+            } else {
+                this.remove(organizerLocation, material);
+                return null;
+            }
         }
 
         return null;
     }
 
-    public synchronized Location put(Location organizerLocation, Material material, Location receiverLocation) {
-        Map<Material, Location> materialMap = this.holder.computeIfAbsent(organizerLocation, k -> new HashMap<>());
-        return materialMap.put(material, receiverLocation);
+    synchronized Location put(Location organizerLocation, Material material, Location receiverLocation) {
+        Map<Material, CacheItem<Location>> materialMap = this.holder.computeIfAbsent(organizerLocation, k -> new HashMap<>());
+        return materialMap.put(material, new CacheItem<>(receiverLocation)).item;
     }
 
-    public synchronized void remove(Location organizerLocation, Material material) {
-        Map<Material, Location> materialMap = this.holder.get(organizerLocation);
+    synchronized void remove(Location organizerLocation, Material material) {
+        Map<Material, CacheItem<Location>> materialMap = this.holder.get(organizerLocation);
         if (materialMap != null) {
             materialMap.remove(material);
         }
+    }
+}
+
+class CacheItem<T> {
+    T item;
+    long time;
+
+    CacheItem(T item) {
+        this.item = item;
+        this.time = System.currentTimeMillis();
     }
 }
